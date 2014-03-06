f2bde793a0405ef25f74742b3a1b4e96651e7648
Rheik van Eyck <rheikvaneyck@yahoo.de>
2014-03-06 22:31:34 +0100

    fixed loadbmp.c
    missed declaration of function

--- c-code/loadbmp.c
+++ c-code/loadbmp.c
@@ -1,28 +1,27 @@
 /*##############################################################*/
-/* loadbmp.c 																									  */
-/* Author: Marcus Nasarek																				*/
+/* 																															*/
 /* From file: tft_test.c	bv																		*/
 /* Author	: 	Hagen Ploog   																		*/
 /*		  	  	Kai Gillmann																			*/
 /*		  	  	Timo Pfander																			*/
-/* License:																											*/
-/*																															*/
-/*	This program is free software; you can redistribute it 			*/ 
-/*	and/or modify it under the terms of the GNU General					*/ 	
-/*	Public License as published by the Free Software 						*/
-/*	Foundation; either version 3 of the License, or 						*/
-/*	(at your option) any later version. 												*/
-/*    																													*/
-/*	This program is distributed in the hope that it will 				*/
-/*	be useful, but WITHOUT ANY WARRANTY; without even the 			*/
-/*	implied warranty of MERCHANTABILITY or 											*/
-/*	FITNESS FOR A PARTICULAR PURPOSE. See the GNU General 			*/
-/*	Public License for more details. 														*/
-/*																															*/
-/*	You should have received a copy of the GNU General 					*/
-/*	Public License along with this program; if not, 						*/
-/*	see <http://www.gnu.org/licenses/>.													*/
-/*																															*/
+/* License:														*/
+/*																*/
+/*	This program is free software; you can redistribute it 		*/ 
+/*	and/or modify it under the terms of the GNU General			*/ 	
+/*	Public License as published by the Free Software 			*/
+/*	Foundation; either version 3 of the License, or 			*/
+/*	(at your option) any later version. 						*/
+/*    															*/
+/*	This program is distributed in the hope that it will 		*/
+/*	be useful, but WITHOUT ANY WARRANTY; without even the 		*/
+/*	implied warranty of MERCHANTABILITY or 						*/
+/*	FITNESS FOR A PARTICULAR PURPOSE. See the GNU General 		*/
+/*	Public License for more details. 							*/
+/*																*/
+/*	You should have received a copy of the GNU General 			*/
+/*	Public License along with this program; if not, 			*/
+/*	see <http://www.gnu.org/licenses/>.							*/
+/*																*/
 /*##############################################################*/
 
 #include <bcm2835.h>
@@ -37,10 +36,19 @@
 #include "loadbmp.h"
 
 
+// load and depict a BMP file
+// ---------------------------------------------
+void CB_DepictBMP( char const *file_name  )
+{
+	uint16_t picture[1][ PICTURE_PIXELS ];
+	
+	Read_bmp2memory ( file_name, &picture[0][ PICTURE_PIXELS-1 ] );
+	RAIO_Write_Picture ( &picture[0][0], PICTURE_PIXELS );	
+}
+
 int main( int argc, char **argv )
 {	
 	if(!argv[1])
-	 printf("first argument must be a 320x240x24bit bitmap file without a compressed header");
 	 return 1;
 		
 	char *my_filename = malloc(strlen(argv[1]));
