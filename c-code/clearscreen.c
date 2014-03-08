3a96248ecd92b608749421799986bcb379be3d45
Marcus Nasarek <m.nasarek@gmx.de>
2014-03-08 22:12:42 +0100

    added clearsceen

---
+++ c-code/clearscreen.c
@@ -0,0 +1,8 @@
+#include "clearscreen.h"
+
+int main( int argc, char **argv ) { 
+  
+  TFT_init_board();
+  TFT_hard_reset();
+
+}
---
+++ c-code/clearscreen.h
@@ -0,0 +1,8 @@
+#ifndef WRITETEXT_H
+#define WRITETEXT_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include "tft.h"
+
+#endif
