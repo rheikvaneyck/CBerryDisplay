29e308d3536cc738b0e33338b9ba560769246ec1
Rheik van Eyck <rheikvaneyck@yahoo.de>
2014-03-21 21:56:02 +0100

    added comments to script
    habe eine Erklärung zum Skript embed_in template.sh hinzugefügt

--- scripts/embed_in_template.sh
+++ scripts/embed_in_template.sh
@@ -4,6 +4,13 @@
 # Version: 1.0
 # Usage: embed_in_template.sh jpg
 # Result: 320x240 pixel x 24 bit Bitmap with jpg included
+# jpg is scaled to a 135 Pixel width and 160 Pixel height 
+# at position 165 Pixel from the left side and 60 Pixel from top
+# de
+# Aufruf: embed_in_template.sh jpg
+# Ergebnis: 320x240 Pixel x 24 bit Bitmap mit eingefügtem jpg
+# Das jpg wird auf 135 Pixel Breite und 160 Pixel Höhe skaliert
+# und an Position 165  Pixel von links und 60 Pixel von oben gesetzt
 
 if [[ -z $(which convert) ]]; then
   echo "Ist ImageMagick nicht installiert?"
