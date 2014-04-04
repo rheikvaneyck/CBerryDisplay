82dd78240c7e352fdfc26354b6ed92420e52229c
Rheik van Eyck <rheikvaneyck@yahoo.de>
2014-04-04 17:07:31 +0200

    refactored primitives

---
+++ c-code/cberry_primitives.c
@@ -0,0 +1,33 @@
+/* cberry_primitives.c
+*
+* Description:
+* 	C lib with functions for drawing and text on C-Berry admatec display 
+*
+* Author: 
+* 	Marcus Nasarek
+*
+* Copyright (C) 2014 Marcus Nasarek
+*
+* Version: 
+* 	0.1 
+*
+* Date:
+* 	2014/03/27
+*
+* Comment:
+* 	Thanks to Hagen Ploog, Kai Gillmann, Timo Pfander at admatec GmbH,
+* 	this file bases on their RAIO8870.c
+*
+* License:
+*	This program is free software; you can redistribute it 
+*	and/or modify it under the terms of the GNU General 	
+*	Public License as published by the Free Software
+*	Foundation. See LICENSE.TXT for further information.
+* 
+* Revision Hiostory:
+* 	Version 0.1 - Initial Version
+*/
+#include <bcm2835.h>
+#include "tft.h"
+#include "cberry_primitives.h"
+
